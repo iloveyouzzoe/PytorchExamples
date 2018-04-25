d6933ef5600f3ed92751a0d4a61a2f3649a13685
linonymous <iamlinonymous@gmail.com>
2018-04-25 20:28:02 +0530

    - add test loss and accuracy

--- Binary Classification/diabetes_clssification.py
+++ Binary Classification/diabetes_clssification.py
@@ -41,7 +41,7 @@ class DiabetesTest(Dataset):
         return self.data[item, :], self.label[item]
 
     def __len__(self):
-        return self.dataset.shape[0]
+        return len(self.data[:, :])
 
 
 class Model(Module):
@@ -49,21 +49,22 @@ class Model(Module):
     def __init__(self, input, hidden, output):
         Module.__init__(self)
         self.layer1 = torch.nn.Linear(input, hidden)
-        self.layer2 = torch.nn.Linear(hidden, output)
+        self.layer1_5 = torch.nn.Linear(hidden, 23)
+        self.layer2 = torch.nn.Linear(23, output)
         self.relu = torch.nn.ReLU()
         self.sigmoid = torch.nn.Sigmoid()
 
     def forward(self, input):
-        out1 = self.sigmoid(self.layer1(input))
-        out2 = self.layer2(out1)
-        return self.sigmoid(out2)
+        out1 = self.relu(self.layer1(input))
+        out2 = self.relu(self.layer1_5(out1))
+        return self.sigmoid(self.layer2(out2))
 
 
 if __name__ == "__main__":
 
     # seed values
     np.random.seed(100)
-    torch.manual_seed(777)
+    torch.manual_seed(888)
 
     # Model params
     input = 13
@@ -72,17 +73,18 @@ if __name__ == "__main__":
     batch_size = 32
 
     # Load train and test data set
-    diabetes_train = DiabetesTrain(path="C:\\Users\\Swapnil.Walke\\PycharmProjects\\PytorchExamples\\diabetes.csv")
-    diabetes_test = DiabetesTest(path="C:\\Users\\Swapnil.Walke\\PycharmProjects\\PytorchExamples\\diabetes.csv")
+    diabetes_train = DiabetesTrain(path="C:\\Users\\Swapnil.Walke\\PycharmProjects\\PytorchExamples\\diabetes_norm.csv")
+    diabetes_test = DiabetesTest(path="C:\\Users\\Swapnil.Walke\\PycharmProjects\\PytorchExamples\\diabetes_norm.csv")
     diabetes_train = DataLoader(diabetes_train, batch_size=batch_size, shuffle=True, num_workers=1)
-    diabetes_test = DataLoader(diabetes_test, batch_size=batch_size, shuffle=True, num_workers=1)
+    diabetes_test = DataLoader(diabetes_test, batch_size=1, shuffle=True, num_workers=1)
 
     # initialize model, criterion and optimizer
     model = Model(input, hidden, output)
-    criterion = torch.nn.BCELoss()
+    criterion = torch.nn.BCELoss(size_average=True)
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
 
-    for epoch in range(15):
+    # Train the model
+    for epoch in range(20):
         loss = 0.0
         for index, data in enumerate(diabetes_train):
             data, label = data
@@ -93,5 +95,32 @@ if __name__ == "__main__":
             loss += criterion(y_pred, y)
             loss.backward(retain_graph=True)
             optimizer.step()
-        loss = (loss/batch_size)
-        print(f' Iteration {epoch} : {loss.data[0]}')
+        train_loss = (loss/len(diabetes_train))
+        loss = 0
+        for index, data in enumerate(diabetes_test):
+            data, label = data
+            x = Variable(data.float())
+            y = Variable(label.float())
+            y_pred = model(x)
+            loss += criterion(y_pred, y)
+        test_loss = (loss/len(diabetes_test))
+        print(f'{epoch} : train [{train_loss.data[0]}] Test [{test_loss.data[0]}]')
+
+    # Calculate the accuracy
+    cnt = 0.0
+    tot = 0
+    for index,data in enumerate(diabetes_test):
+        data, label = data
+        x = Variable(data.float())
+        y = Variable(label.float())
+        y_pred = model(x)
+        pred = y_pred.data.numpy()[0][0]
+        var = y.data.numpy()[0]
+        tot += 1
+        if pred > 0.5:
+            pred = 1.0
+        else:
+            pred = 0.0
+        if pred == var:
+            cnt += 1
+    print(f'Accuracy : {cnt/tot}')
